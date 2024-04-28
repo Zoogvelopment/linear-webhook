50c65a3394db9bfed85d82464577f0f709300ac2
Andrew Brower <bigspeedfpv@gmail.com>
2024-04-28 04:22:16 -0400

    fix: use digestif, forwarded ip from cloudflared

--- bin/main.ml
+++ bin/main.ml
@@ -1,26 +1,30 @@
 let linear_ips = [ "35.231.147.226"; "35.243.134.228" ]
-let webhook_secret = "WEBHOOK_SECRET" |> Sys.getenv |> Cstruct.of_string
 
-let request_is_valid body req =
+let request_is_valid secret body req =
   let verify_signature ls =
-    let open Mirage_crypto.Hash.SHA256 in
-    let digest =
-      body |> Cstruct.of_string |> hmac ~key:webhook_secret
-      |> Cstruct.to_hex_string
-    in
+    let open Digestif.SHA256 in
+    let digest = body |> hmac_string ~key:secret |> to_hex in
+    Dream.debug (fun m -> m "Body signature is %s" digest);
     digest = ls
   in
-  let ip = Dream.client req in
+  let ip =
+    match Dream.header req "X-Forwarded-For" with
+    | Some ip -> ip
+    | None -> Dream.client req
+  in
+  Dream.debug (fun m -> m "IP: %s" ip);
   match List.mem ip linear_ips with
   | false -> false
   | true -> (
-      match Dream.header req "linear-signature" with
+      match Dream.header req "Linear-Signature" with
       | None -> false
-      | Some linear_signature -> verify_signature linear_signature)
+      | Some linear_signature ->
+          Dream.debug (fun m -> m "Linear signature is %s" linear_signature);
+          verify_signature linear_signature)
 
-let forward_webhook req =
+let forward_webhook secret req =
   let%lwt body = Dream.body req in
-  match request_is_valid body req with
+  match request_is_valid secret body req with
   | false -> Dream.empty `Bad_Request
   | true ->
       let%lwt body = Dream.body req in
@@ -28,10 +32,12 @@ let forward_webhook req =
       Dream.empty `OK
 
 let () =
+  Dotenv.export () |> ignore;
+  let webhook_secret = "WEBHOOK_SECRET" |> Sys.getenv in
   Dream.initialize_log ~level:`Debug ();
   Dream.run @@ Dream.logger
   @@ Dream.router
        [
          Dream.get "/" (fun _ -> Dream.respond "OK");
-         Dream.post "/" forward_webhook;
+         Dream.post "/" (forward_webhook webhook_secret);
        ]
--- flake.nix
+++ flake.nix
@@ -49,13 +49,44 @@
             inherit version;
             pname = "okitten";
             propagatedBuildInputs = with ocamlPackages; [
+              pkgs.cloudflared
+
               dream
               lwt_ppx
               ppx_deriving_yojson
-              mirage-crypto
+              digestif
               iso8601
+              self'.packages.dotenv
             ];
             src = ./.;
+          };
+
+          dotenv = buildDunePackage {
+            version = "0.0.3";
+            pname = "dotenv";
+            src = builtins.fetchGit {
+              url = "https://github.com/thatportugueseguy/ocaml-dotenv";
+              rev = "972f7e61e7783f0a3185eeca34d9bf4d1acd43c4";
+            };
+            propagatedBuildInputs = with ocamlPackages; [
+              base
+              stdio
+              uutf
+              self'.packages.pcre
+            ];
+          };
+
+          pcre = buildDunePackage {
+            version = "7.5.0";
+            pname = "pcre";
+            src = builtins.fetchGit {
+              url = "https://github.com/mmottl/pcre-ocaml";
+              rev = "cd2a7ba634348015fc304a9d7efdec0239285d14";
+            };
+            propagatedBuildInputs = with ocamlPackages; [
+              pkgs.pcre
+              dune-configurator
+            ];
           };
         };
       };
